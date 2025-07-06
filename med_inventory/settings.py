d7feaa531561cca64695da0eb7fc5445f370b2ee
SanChainn <sanchaintunucsy@gmail.com>
2025-07-07 01:05:23 +0900

    Initial commit of django med inventory project

--- med_inventory/settings.py
+++ med_inventory/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +78,42 @@ WSGI_APPLICATION = 'med_inventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'pharmacy_db',          # The name you created
+#         'USER': 'pharmacy_user',         # The user you created
+#         'PASSWORD': 'a_very_secure_password', # The password you set
+#         'HOST': 'localhost',             # Or your DB host
+#         'PORT': '5432',                  # Default port
+#     }
+# }
+
+
+if os.environ.get('USE_POSTGRES', '') == 'True':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
